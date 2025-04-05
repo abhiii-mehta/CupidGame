6858e0c8e86084930127d983b16b2ae742b49f2b
Abhinandan <abhimehta1324@gmail.com>
2025-04-05 17:04:12 +0200

    menus placeholder added

---
+++ CupidGame/Assets/Scripts/InGameMenuManager.cs
@@ -0,0 +1,44 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenuManager : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject gameOverPanel;
+
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        pausePanel.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    public void ShowGameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ CupidGame/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    private void Start()
+    {
+        masterSlider.onValueChanged.AddListener(SetMasterVolume);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1)) * 20);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1)) * 20);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1)) * 20);
+    }
+}
