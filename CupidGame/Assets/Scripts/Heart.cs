16920cf0bf2ab12d13dd60004947fab7ab9b6894
Abhinandan <abhimehta1324@gmail.com>
2025-04-05 16:54:32 +0200

    win and lose conditions added

---
+++ CupidGame/Assets/Scripts/Heart.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    public enum HeartColor
+    {
+        Red,
+        Blue,
+        Yellow
+    }
+
+    public HeartColor heartColor;
+}
